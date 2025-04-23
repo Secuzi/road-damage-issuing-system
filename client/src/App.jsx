9d7cff52a66ba52d78561ac6e553652ae448c393
Secuzi <secre2k@gmail.com>
2025-04-23 20:24:51 +0800

    Project initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div>
+      <button class="btn">Button</button>
+      <input
+        type="checkbox"
+        value="light"
+        className="toggle theme-controller"
+      />
+    </div>
+  );
+}
