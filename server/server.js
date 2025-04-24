131724d32b0f58a50404d4c987f92575099633e9
Tacs <act.rtacangjr@gmail.com>
2025-04-24 20:10:55 +0800

    chore: update dependencies

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 import 'dotenv/config'
 import express from 'express'
 import cookieParser from 'cookie-parser'
-//import cors from 'cors'
+import cors from 'cors'
 
 const app = express()
 
-// app.use(
-//     cors({
-//         origin: process.env.CLIENT_URL,
-//         credentials: true,
-//     }
-// )
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+)
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
