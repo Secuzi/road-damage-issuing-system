4755385e376dc88e12eb13f5d2bb44844b4396ac
Tacs <act.rtacangjr@gmail.com>
2025-04-24 12:47:07 +0800

    Added auth route boiletplate

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+//import cors from 'cors'
+
+const app = express()
+
+// app.use(
+//     cors({
+//         origin: process.env.CLIENT_URL,
+//         credentials: true,
+//     }
+// )
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+import authRoute from './routes/auth.routes.js'
+app.use('/api/auth', authRoute) //http://localhost:3000/api/auth
+
+const port = process.env.PORT || 3000
+app.listen(port)
