593c0029a38b6a9cd0c9ebe6bf43725b4987156f
Secuzi <secre2k@gmail.com>
2025-04-26 21:19:34 +0800

    feat: Added verifyEmail function

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -6,6 +6,7 @@ import {
   register,
   logout,
   sendVerification,
+  verifyEmail,
 } from "../controllers/auth.controller.js";
 import userAuth from "../middlewares/auth.middleware.js";
 import { sendEmail } from "../utils/mail.js";
@@ -16,7 +17,7 @@ router.post("/login", validate(loginSchema), login);
 router.post("/register", validate(registerSchema), register);
 router.get("/logout", logout);
 router.post("/send-email-otp", userAuth, sendVerification);
-
+router.post("/verify-email", userAuth, verifyEmail);
 // Development routes
 router.get("/protected", userAuth, (req, res, next) => {
   return res.json({ message: req.user });
