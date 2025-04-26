8721f3a70b7e751802a97645a03ac819df390a94
Secuzi <secre2k@gmail.com>
2025-04-26 17:29:36 +0800

    Added logout function to the auth routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import validate from "../middlewares/zodValidate.js";
 import { loginSchema, registerSchema } from "../schema/zod.schema.js";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, register, logout } from "../controllers/auth.controller.js";
 import userAuth from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/login", validate(loginSchema), login);
 router.post("/register", validate(registerSchema), register);
-
+router.get("/logout", logout);
 // Development routes
 router.get("/protected", userAuth, (req, res) => {
   return res.json({ message: req.user });
