632bfa063ab7079de1d35d94fe1eadcbe80e075f
Secuzi <secre2k@gmail.com>
2025-04-26 20:07:18 +0800

    Added sendverification function for routes
    Added development route for email testing

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,16 +1,34 @@
 import express from "express";
 import validate from "../middlewares/zodValidate.js";
 import { loginSchema, registerSchema } from "../schema/zod.schema.js";
-import { login, register, logout } from "../controllers/auth.controller.js";
+import {
+  login,
+  register,
+  logout,
+  sendVerification,
+} from "../controllers/auth.controller.js";
 import userAuth from "../middlewares/auth.middleware.js";
+import { sendEmail } from "../utils/mail.js";
 
 const router = express.Router();
 
 router.post("/login", validate(loginSchema), login);
 router.post("/register", validate(registerSchema), register);
 router.get("/logout", logout);
+router.post("/send-email-otp", userAuth, sendVerification);
+
 // Development routes
-router.get("/protected", userAuth, (req, res) => {
+router.get("/protected", userAuth, (req, res, next) => {
   return res.json({ message: req.user });
 });
+router.post("/smtp", async (req, res, next) => {
+  const output = await sendEmail(
+    "act.rtacangjr@gmail.com",
+    "Subject Header",
+    "Bomambini di pinili 😭, tralalelo tropa lang 😔",
+    next
+  );
+  return res.json(output);
+});
+
 export default router;
