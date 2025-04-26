ebd1b34eafc3804cc7b3d8791f55ac8b1a3c0957
Secuzi <secre2k@gmail.com>
2025-04-26 17:28:59 +0800

    Added clearCookie function

--- server/utils/cookie.js
+++ server/utils/cookie.js
@@ -1,8 +1,12 @@
 export const createCookie = (res, name, token, expiry) => {
-    res.cookie(name, token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        maxAge: expiry,
-    })
-}
+  res.cookie(name, token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: expiry,
+  });
+};
+
+export const clearCookie = (res, name) => {
+  res.clearCookie(name);
+};
