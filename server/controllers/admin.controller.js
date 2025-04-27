95dd5b7928a9c67cae108818f6c6590604784a38
Tacs <act.rtacangjr@gmail.com>
2025-04-27 20:53:21 +0800

    fix: Added next in admin controller

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -6,52 +6,52 @@ import {
 } from '../services/issue.service.js'
 import {fetchUsers, fetchUserById} from '../services/user.service.js'
 
-export const getIssues = async (req, res) => {
+export const getIssues = async (req, res, next) => {
     const issues = await fetchIssues()
 
     res.status(200).json(issues)
 }
 
-export const getIssue = async (req, res) => {
+export const getIssue = async (req, res, next) => {
     const {id} = req.params
 
     const issue = await fetchIssueById(id)
-    if (!issue) return createError(404, 'Issue not found')
+    if (!issue) return next(createError(404, 'Issue not found'))
 
     res.status(200).json(issue)
 }
 
-export const updateIssue = async (req, res) => {
+export const updateIssue = async (req, res, next) => {
     const {id} = req.params
 
     res.send(`Update Issue ${id}`)
 }
 
-export const deleteIssue = async (req, res) => {
+export const deleteIssue = async (req, res, next) => {
     const {id} = req.params
 
     const result = await deleteIssueById(id)
-    if (!result) return createError(404, 'Issue not found')
+    if (!result) return next(createError(404, 'Issue not found'))
 
     res.status(200).json({message: 'Issue deleted'})
 }
 
-export const getUsers = async (req, res) => {
+export const getUsers = async (req, res, next) => {
     const users = await fetchUsers()
 
     res.status(200).json(users)
 }
 
-export const getUser = async (req, res) => {
+export const getUser = async (req, res, next) => {
     const {id} = req.params
 
     const user = await fetchUserById(id)
-    if (!user) return createError(404, 'User not found')
+    if (!user) return next(createError(404, 'User not found'))
 
     res.status(200).json(user)
 }
 
-export const suspendUser = async (req, res) => {
+export const suspendUser = async (req, res, next) => {
     const {id} = req.params
     res.send(`Suspend User ${id}`)
 }
