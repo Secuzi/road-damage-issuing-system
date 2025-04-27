13fea39c30e95260681b8bdb0b0eaafd2b9de021
Tacs <act.rtacangjr@gmail.com>
2025-04-27 19:56:27 +0800

    feat: Added user controller logic

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,15 +1,27 @@
+import createError from 'http-errors'
+import {
+    fetchIssues,
+    fetchIssueById,
+    insertIssue,
+    updateIssueById,
+    deleteIssueById,
+} from '../services/issue.service.js'
+
 export const getIssues = async (req, res) => {
-    res.send('getIssues')
+    const issues = await fetchIssues()
+    res.status(200).send(issues)
 }
 
 export const getIssue = async (req, res) => {
     const {id} = req.params
-    res.send(`getIssue ${id}`)
+
+    const issue = await fetchIssueById(id)
+    if (!issue) return createError(404, 'Issue not found')
+
+    res.status(200).send(issue)
 }
 
-export const createIssue = async (req, res) => {
-    res.send('createIssue')
-}
+export const createIssue = async (req, res) => {}
 
 export const updateIssue = async (req, res) => {
     const {id} = req.params
