ca410b1396e6d78bb25753a477f059496e2cf0c0
Tacs <act.rtacangjr@gmail.com>
2025-04-27 13:34:59 +0800

    refactor: Changed guest controller folder structure

--- server/routes/guest.route.js
+++ server/routes/guest.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import catchAsync from '../utils/catchAsync.js'
-import {getIssues, getIssue} from '../controllers/guest/issue.guest.js'
+import {getIssues, getIssue} from '../controllers/guest.controller.js'
 
 const router = express.Router()
 
