f217adfb72f03f5d32042fafe8a1a81e748ba3d3
Tacs <act.rtacangjr@gmail.com>
2025-04-27 20:57:52 +0800

    refactor: Added services to the guest controller

--- server/controllers/guest.controller.js
+++ server/controllers/guest.controller.js
@@ -1,16 +1,18 @@
-import Issue from '../models/issue.model.js'
+import createError from 'http-errors'
+import {fetchIssues, fetchIssueById} from '../services/issue.service.js'
 
-export const getIssues = async (req, res) => {
-    const issues = await Issue.find()
+export const getIssues = async (req, res, next) => {
+    const issues = await fetchIssues()
 
-    console.log(issues[0]._id)
+    if (issues.length === 0) return next(createError(404, 'No issues found'))
     res.status(200).json(issues)
 }
 
-export const getIssue = async (req, res) => {
+export const getIssue = async (req, res, next) => {
     const {id} = req.params
 
-    const issue = await Issue.findById(id)
+    const issue = await fetchIssueById(id)
+    if (!issue) return next(createError(404, 'Issue not found'))
 
     res.status(200).json(issue)
 }
