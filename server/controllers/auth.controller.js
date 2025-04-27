8bc11716aa2a87cb794d567273929e444592acce
Secuzi <secre2k@gmail.com>
2025-04-27 10:03:19 +0800

    refactor: Refactored try-catch block in controllers

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -8,41 +8,36 @@ import { generateOtp } from "../utils/generateOtp.js";
 import { sendEmail } from "../utils/mail.js";
 
 export const login = async (req, res, next) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return next(createError(404, "User not found"));
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return next(createError(404, "User not found"));
 
-    const isPasswordMatched = await isPasswordMatch(
-      req.body.password,
-      user.password
-    );
-    if (!isPasswordMatched) return next(createError(400, "Invalid password"));
+  const isPasswordMatched = await isPasswordMatch(
+    req.body.password,
+    user.password
+  );
+  if (!isPasswordMatched) return next(createError(400, "Invalid password"));
 
-    const payload = {
-      id: user._id,
-      email: user.email,
-      role: user.role,
-    };
+  const payload = {
+    id: user._id,
+    email: user.email,
+    role: user.role,
+  };
 
-    const accessToken = generateAccessToken(
-      payload,
-      process.env.ACCESS_TOKEN_SECRET
-    );
-    const refreshToken = generateRefreshToken(
-      payload,
-      process.env.REFRESH_TOKEN_SECRET
-    );
+  const accessToken = generateAccessToken(
+    payload,
+    process.env.ACCESS_TOKEN_SECRET
+  );
+  const refreshToken = generateRefreshToken(
+    payload,
+    process.env.REFRESH_TOKEN_SECRET
+  );
 
-    createCookie(res, "accessToken", accessToken, minutesToMilliseconds(15));
-    createCookie(res, "refreshToken", refreshToken, daysToMilliseconds(7));
+  createCookie(res, "accessToken", accessToken, minutesToMilliseconds(15));
+  createCookie(res, "refreshToken", refreshToken, daysToMilliseconds(7));
 
-    res.status(200).json({
-      message: "Login successful",
-    });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+  res.status(200).json({
+    message: "Login successful",
+  });
 };
 
 export const register = async (req, res, next) => {
@@ -55,44 +50,39 @@ export const register = async (req, res, next) => {
       "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200",
   };
 
-  try {
-    const isUserExist = await User.findOne({ email: req.body.email });
-    if (isUserExist) return next(createError(400, "User already exists"));
+  const isUserExist = await User.findOne({ email: req.body.email });
+  if (isUserExist) return next(createError(400, "User already exists"));
 
-    const userData = {
-      ...req.body,
-    };
-    const hashedPassword = await hashPassword(userData.password);
-    userData.password = hashedPassword;
-    await User.create(userData);
+  const userData = {
+    ...req.body,
+  };
+  const hashedPassword = await hashPassword(userData.password);
+  userData.password = hashedPassword;
+  await User.create(userData);
 
-    const user = await User.findOne(
-      { email: userData.email },
-      { email: true, role: true }
-    );
+  const user = await User.findOne(
+    { email: userData.email },
+    { email: true, role: true }
+  );
 
-    const payload = {
-      id: user._id,
-      email: user.email,
-      role: user.role,
-    };
+  const payload = {
+    id: user._id,
+    email: user.email,
+    role: user.role,
+  };
 
-    const accessToken = generateAccessToken(
-      payload,
-      process.env.ACCESS_TOKEN_SECRET
-    );
-    const refreshToken = generateRefreshToken(
-      payload,
-      process.env.REFRESH_TOKEN_SECRET
-    );
-    createCookie(res, "accessToken", accessToken, minutesToMilliseconds(15));
-    createCookie(res, "refreshToken", refreshToken, daysToMilliseconds(7));
+  const accessToken = generateAccessToken(
+    payload,
+    process.env.ACCESS_TOKEN_SECRET
+  );
+  const refreshToken = generateRefreshToken(
+    payload,
+    process.env.REFRESH_TOKEN_SECRET
+  );
+  createCookie(res, "accessToken", accessToken, minutesToMilliseconds(15));
+  createCookie(res, "refreshToken", refreshToken, daysToMilliseconds(7));
 
-    res.status(201).json({ message: "User created successfully" });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+  res.status(201).json({ message: "User created successfully" });
 };
 
 export const logout = async (req, res) => {
@@ -105,29 +95,29 @@ export const logout = async (req, res) => {
 export const sendVerification = async (req, res, next) => {
   const { id } = req.user;
 
-  try {
-    const user = await User.findById(id);
-    if (!user) {
-      return next(createError(404, "User could not be found"));
-    }
-
-    const otp = generateOtp();
-
-    user.verificationOtp = otp;
-
-    user.verificationOtpExpireAt = Date.now() + minutesToMilliseconds(1);
-    await user.save();
-
-    const output = await sendEmail(
-      user.email,
-      "Email Verification",
-      `Thank you for verifying your account. Your one-time password (OTP) is: ${otp}`,
-      next
-    );
-    return res.status(200).json(output);
-  } catch (error) {
-    return next(createError(404, error.message));
+  const user = await User.findById(id);
+  if (!user) {
+    return next(createError(404, "User could not be found"));
   }
+
+  if (user.isAccountVerified) {
+    return next(createError(404, "Account has already been verified"));
+  }
+
+  const otp = generateOtp();
+
+  user.verificationOtp = otp;
+
+  user.verificationOtpExpireAt = Date.now() + minutesToMilliseconds(1);
+  await user.save();
+
+  const output = await sendEmail(
+    user.email,
+    "Email Verification",
+    `Thank you for verifying your account. Your one-time password (OTP) is: ${otp}`,
+    next
+  );
+  return res.status(200).json(output);
 };
 
 export const verifyEmail = async (req, res, next) => {
@@ -137,27 +127,23 @@ export const verifyEmail = async (req, res, next) => {
     return next(createError(400, "OTP must be supplied."));
   }
 
-  try {
-    const user = await User.findById(id);
-    if (!user) {
-      return next(createError(404, "User could not be found"));
-    }
-
-    if (user.verificationOtp !== otp || user.verificationOtp === "") {
-      return next(createError(404, "Invalid OTP"));
-    }
-
-    if (user.verificationOtpExpireAt < Date.now()) {
-      return next(createError(404, "OTP has expired"));
-    }
-
-    user.isAccountVerified = true;
-    user.verificationOtp = "";
-    user.verificationOtpExpireAt = 0;
-    await user.save();
-
-    return res.status(200).json({ message: "Account verified" });
-  } catch (error) {
-    return next(createError(404, error.message));
+  const user = await User.findById(id);
+  if (!user) {
+    return next(createError(404, "User could not be found"));
   }
+
+  if (user.verificationOtp !== otp || user.verificationOtp === "") {
+    return next(createError(404, "Invalid OTP"));
+  }
+
+  if (user.verificationOtpExpireAt < Date.now()) {
+    return next(createError(404, "OTP has expired"));
+  }
+
+  user.isAccountVerified = true;
+  user.verificationOtp = "";
+  user.verificationOtpExpireAt = 0;
+  await user.save();
+
+  return res.status(200).json({ message: "Account verified" });
 };
