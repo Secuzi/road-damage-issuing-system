166a82ea6a65bfb0b53f5fad0e03ce42ce0ad486
Secuzi <secre2k@gmail.com>
2025-04-28 20:19:20 +0800

    fix: Syntax error

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,21 +1,21 @@
-import {useState} from 'react'
-import {AppContentContext} from '../utils/context'
+import { useState } from "react";
+import { AppContentContext } from "../utils/context";
 
-export default function AppContentContextProvider({children}) {
-    const backEndURL = process.meta.env.BACKEND_URL
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-    const [userData, setUserData] = useState({})
-    const values = {
-        isLoggedIn,
-        setIsLoggedIn,
-        backEndURL,
-        userData,
-        setUserData,
-    }
+export default function AppContentContextProvider({ children }) {
+  const backEndURL = import.meta.env.BACKEND_URL;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState({});
+  const values = {
+    isLoggedIn,
+    setIsLoggedIn,
+    backEndURL,
+    userData,
+    setUserData,
+  };
 
-    return (
-        <AppContentContext.Provider value={values}>
-            {children}
-        </AppContentContext.Provider>
-    )
+  return (
+    <AppContentContext.Provider value={values}>
+      {children}
+    </AppContentContext.Provider>
+  );
 }
