3ce75f18b78f8e47a6751d486629b79565a59f92
Tacs <act.rtacangjr@gmail.com>
2025-04-30 09:38:19 +0800

    fix: Added file extension to the file name

--- server/configs/upload.config.js
+++ server/configs/upload.config.js
@@ -5,8 +5,9 @@ const storage = multer.diskStorage({
         cb(null, 'uploads/')
     },
     filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname)
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-        cb(null, file.fieldname + '-' + uniqueSuffix)
+        cb(null, file.fieldname + '-' + uniqueSuffix + ext)
     },
 })
 
