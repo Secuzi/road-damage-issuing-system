e0721338904f61311fe9097f6ba853cc74d248c5
Tacs <act.rtacangjr@gmail.com>
2025-05-03 19:53:52 +0800

    feat: Added hook form integration in login page

--- client/src/hooks/useLoginForm.jsx
+++ client/src/hooks/useLoginForm.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import {useForm} from 'react-hook-form'
 import {zodResolver} from '@hookform/resolvers/zod'
-import {loginSchema} from '../schemas/zod/authSchema'
+import {loginSchema} from '../schemas/authSchema'
 
 export default function useLoginForm() {
     return useForm({
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -3,8 +3,11 @@ import TextField from '../components/TextField'
 import Button from '../components/Button'
 import {Link} from 'react-router-dom'
 import Checkbox from '../components/Checkbox'
+import useLoginForm from '../hooks/useLoginForm'
 
 export default function LoginPage() {
+    const {control, handleSubmit} = useLoginForm()
+
     return (
         <form className="w-[350px] flex flex-col item py-4 px-8 rounded-xl shadow-md shadow-slate-400 text-font-color">
             <h2 className="my-4 text-2xl font-bold text-center">Login</h2>
