e555e8b5b9169cf7dab6a956a272301d492a735d
Tacs <act.rtacangjr@gmail.com>
2025-05-03 19:40:02 +0800

    feat: Added custom form hook

--- client/src/hooks/useLoginForm.jsx
+++ client/src/hooks/useLoginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
 import {zodResolver} from '@hookform/resolvers/zod'
-import loginSchema from '../schema/zod/loginSchema'
+import {loginSchema} from '../schemas/zod/authSchema'
 
 export default function useLoginForm() {
     return useForm({
