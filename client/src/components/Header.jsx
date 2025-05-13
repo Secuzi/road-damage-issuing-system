2b833b3d373abc61f77b3691205427fc025c7560
Secuzi <secre2k@gmail.com>
2025-05-13 09:11:19 +0800

    feat: changed h2 to Link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ThemeController from './ThemeController'
+import React from "react";
+import ThemeController from "./ThemeController";
+import { Link } from "react-router-dom";
 
-export default function Header({children}) {
-    return (
-        <header className="flex flex-row justify-around itnems-center p-8">
-            <h2 className="flex-[.2] text-lg font-bold">RoadBusters</h2>
-            <nav className="flex-[.5]">{children}</nav>
-            <ThemeController />
-        </header>
-    )
+export default function Header({ children }) {
+  return (
+    <header className="flex flex-row justify-around itnems-center p-8">
+      <Link to="/" className="flex-[.2] text-lg font-bold">
+        RoadBusters
+      </Link>
+      <nav className="flex-[.5]">{children}</nav>
+      <ThemeController />
+    </header>
+  );
 }
