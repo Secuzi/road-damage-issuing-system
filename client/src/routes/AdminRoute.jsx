bdfeb9dade88dc6b7cbbfb77bf9cee26f15597f7
Secuzi <secre2k@gmail.com>
2025-05-13 09:04:24 +0800

    feat: Removed admin and guest routes for frontend and backend

--- client/src/routes/AdminRoute.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function AdminRoute() {
-    return <div>AdminRoute</div>
-}
--- client/src/routes/GuestRoute.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function GuestRoute() {
-    return <div>GuestRoute</div>
-}
