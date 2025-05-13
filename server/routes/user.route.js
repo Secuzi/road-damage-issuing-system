39135bf8998b8a3321690da29b950d7f2acb6644
Secuzi <secre2k@gmail.com>
2025-05-13 09:11:04 +0800

    feat: added routes for users (didn't remove comments because might be useful when creating the issue route)

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,20 +1,24 @@
-import express from 'express'
-import catchAsync from '../utils/catchAsync.js'
+import express from "express";
+import catchAsync from "../utils/catchAsync.js";
 import {
-    getIssues,
-    getIssue,
-    createIssue,
-    updateIssue,
-    deleteIssue,
-} from '../controllers/user.controller.js'
+  getUser,
+  getUsers,
+  suspendUser,
+} from "../controllers/user.controller.js";
+import userAuth from "../middlewares/auth.middleware.js";
 
-const router = express.Router()
-
-router.route('/issues').get(catchAsync(getIssues)).post(catchAsync(createIssue))
-router
-    .route('/issues/:id')
-    .get(catchAsync(getIssue))
-    .put(catchAsync(updateIssue))
-    .delete(catchAsync(deleteIssue))
-
-export default router
+const router = express.Router();
+// http://localhost:3000/api/user
+// router
+//   .route("/issues")
+//   .get(catchAsync(getIssues))
+//   .post(catchAsync(createIssue));
+// router
+//   .route("/issues/:id")
+//   .get(catchAsync(getIssue))
+//   .put(catchAsync(updateIssue))
+//   .delete(catchAsync(deleteIssue));
+router.get("/get-user", userAuth, getUsers);
+router.route("/:id").get(catchAsync(getUser)).get(catchAsync(suspendUser));
+// router.route("/get-user").get(userAuth, catchAsync(getUser));
+export default router;
