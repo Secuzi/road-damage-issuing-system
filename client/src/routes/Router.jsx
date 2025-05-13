af54c7878ef9b7fc8ca4ae26250ee686ffca4179
Secuzi <secre2k@gmail.com>
2025-05-13 09:13:02 +0800

    feat: Added Suspense to HomePage to disable flickering and added HomePageLoader fallback component

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,28 +1,40 @@
-import {createBrowserRouter} from 'react-router-dom'
-import AuthRoute from './AuthRoute'
-import LoginPage from '../pages/LoginPage'
-import RegisterPage from '../pages/RegisterPage'
-import PageNotFound from '../pages/PageNotFound'
-
+import { createBrowserRouter } from "react-router-dom";
+import AuthRoute from "./AuthRoute";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import PageNotFound from "../pages/PageNotFound";
+import { Suspense } from "react";
+import Homepage from "../pages/Homepage";
+import { HomepageLoader } from "../utils/HomepageLoader";
+const HomePageLoader = () => <div>Loading app...</div>;
 const router = createBrowserRouter([
-    {
-        path: '/auth',
-        element: <AuthRoute />,
-        children: [
-            {
-                path: 'login',
-                element: <LoginPage />,
-            },
-            {
-                path: 'register',
-                element: <RegisterPage />,
-            },
-        ],
-    },
-    {
-        path: '*',
-        element: <PageNotFound />,
-    },
-])
+  {
+    path: "/auth",
+    element: <AuthRoute />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<HomePageLoader />}>
+        <Homepage />
+      </Suspense>
+    ),
+    loader: HomepageLoader,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
 
-export default router
+export default router;
